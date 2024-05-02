a530fa3b7e0b48d40507bb5a14331be5842f3449
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-05-01 17:04:25 -0700

    change routes category

--- server/routes/category.routes.ts
+++ server/routes/category.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { createCategory, getCategories, getProductsByCategory } from "../controllers/category.controller";
+import { createCategory, getCategories } from "../controllers/category.controller";
 import { createMark , getAllMarks} from "../controllers/mark.controller";
 
 const router = Router()
 
 router.post('/category', createCategory);
 router.get('/category', getCategories);
-router.get('/category/:category', getProductsByCategory);
+
 router.post('/mark', createMark)
 router.get('/mark', getAllMarks)
 
